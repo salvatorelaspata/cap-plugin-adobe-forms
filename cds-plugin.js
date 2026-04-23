93b34e77950aeb56e287746a4eb6931927215a6a
SalvatoreLa <salvatore.la.spata@gmail.com>
2026-04-23 17:15:28 +0200

    expose method

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,14 @@
 const cds = require('@sap/cds')
 
+// Register the service implementation so cds.connect.to('adobeForms') returns
+// an instance with remoteHealth(), listForms(), renderPDF(), etc.
+cds.on('bootstrap', () => {
+  const requires = cds.env.requires
+  if (requires?.adobeForms) {
+    requires.adobeForms.impl ??= require.resolve('./lib/adobe-service')
+  }
+})
+
 cds.on('served', () => {
   cds.log('adobe-forms-plugin').info('CAP Adobe Forms plugin loaded')
 })
